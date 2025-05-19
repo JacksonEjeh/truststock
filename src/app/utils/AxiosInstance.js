77ec17f39680b22e0321939bff48cc949e963d19
JACKSON <gabbyjax7@gmail.com>
2025-05-19 03:11:17 +0100

    testing without env

--- src/app/utils/AxiosInstance.js
+++ src/app/utils/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const Api = axios.create({
-    baseURL: 'https://truststock-api.onrender.com/api/v1',
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL,
     withCredentials: true,
 });
 
